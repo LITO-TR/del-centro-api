493640538533ea3ddf4e832c8f7c5712eaa9eea3
Nilton Torres <89040283+LITO-TR@users.noreply.github.com>
2023-01-07 00:38:54 -0500

    added post for customer route

--- src/routes/customer.routes.js
+++ src/routes/customer.routes.js
@@ -4,4 +4,5 @@ const customerController = require('../controllers/customer.controller')
 const router = Router()
 
 router.get('/', customerController.getAllCustomers)
+router.post('/', customerController.createCustomer)
 module.exports = router
