7997e28663001ea5a22b0717d645d7f32799f240
Nilton Torres <89040283+LITO-TR@users.noreply.github.com>
2023-01-06 20:14:41 -0500

    changed file names

--- src/routes/customerRoute.js
+++
@@ -1,7 +0,0 @@
-const { Router } = require('express')
-const customerController = require('../controllers/customerController')
-
-const router = Router()
-
-router.get('/', customerController.getAllCustomers)
-module.exports = router
