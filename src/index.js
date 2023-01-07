c86ae68a2b5d8ce60b95f1a12034a87ecd293503
Nilton Torres <89040283+LITO-TR@users.noreply.github.com>
2023-01-06 20:11:00 -0500

    server run

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const dotenv = require('dotenv')
+const mongoose = require('mongoose')
+const router = require('./routes/index.routes')
+dotenv.config()
+
+const app = express()
+app.use(express.json())
+router(app)
+const PORT = process.env.PORT || 9000
+const MONGODB = process.env.MONGO_URI
+
+app.get('/', (req, res) => {
+  res.send('API')
+})
+// connection db
+mongoose
+  .connect(MONGODB)
+  .then(() => console.log('connected'))
+  .catch((error) => console.error(error))
+
+app.listen(PORT, () => {
+  console.log('Server is running | ', PORT)
+})
