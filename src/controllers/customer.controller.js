ecd872bdef783ca83c5ffb999b9542ebb5b654fe
Nilton Torres <89040283+LITO-TR@users.noreply.github.com>
2023-01-07 00:36:29 -0500

    added create customer

--- src/controllers/customer.controller.js
+++ src/controllers/customer.controller.js
@@ -1,8 +1,7 @@
-const customerService = require('../services/customer.service')
-
+const customer = require('../models/customer.model')
 const getAllCustomers = async (req, res) => {
   try {
-    const customers = await customerService.getCustomers()
+    const customers = await customer.find()
     return res.json(
       customers
     )
@@ -14,13 +13,19 @@ const getAllCustomers = async (req, res) => {
 }
 
 const createCustomer = async (req, res) => {
+  const customerData = req.body
+  customerData.createdAt = Date.now()
+  customerData.updateAt = Date.now()
   try {
-    const createdCustomer = customerService.createCustomer(req.body)
-    res.status(200).json(
-      createdCustomer
-    )
+    const createdCustomer = await customer.create(req.body)
+    res.status(200).json({
+      msg: 'customer created',
+      data: createdCustomer
+    })
   } catch (error) {
-
+    res.status(400).send({
+      msg: error
+    })
   }
 }
 
