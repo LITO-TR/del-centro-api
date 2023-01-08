81ff7b95f9670d7514ae06d50e04e8b56dd1ac22
Nilton Torres <89040283+LITO-TR@users.noreply.github.com>
2023-01-08 12:34:34 -0500

    added employee routes

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,5 +1,7 @@
-const customerRoute = require('./customer.routes')
+const employeeRoutes = require('../routes/employee.routes')
+const customerRoutes = require('../routes/customer.routes')
 const routes = (app) => {
-  app.use('/api/customers', customerRoute)
+  app.use('/api/customers', customerRoutes)
+  app.use('/api/employees', employeeRoutes)
 }
 module.exports = routes
