ba37db329e82496bb0ce40358548ff370ae8535e
Nilton Torres <89040283+LITO-TR@users.noreply.github.com>
2023-01-08 12:33:22 -0500

    added get by id, update and delete for customer

--- src/controllers/customer.controller.js
+++ src/controllers/customer.controller.js
@@ -1,7 +1,7 @@
-const customer = require('../models/customer.model')
+const Customer = require('../models/customer.model')
 const getAllCustomers = async (req, res) => {
   try {
-    const customers = await customer.find()
+    const customers = await Customer.find()
     return res.json(
       customers
     )
@@ -12,15 +12,30 @@ const getAllCustomers = async (req, res) => {
   }
 }
 
+const getOneByCustomerId = async (req, res) => {
+  const { id } = req.params
+  try {
+    const customer = await Customer.findById(id)
+    res.status(200).json(
+      customer
+    )
+  } catch (error) {
+    res.status(400).send({
+      msg: error
+    })
+  }
+}
+
 const createCustomer = async (req, res) => {
+  const { name } = req.body // manda en el cuerpo lo extraigo asi
   const customerData = req.body
   customerData.createdAt = Date.now()
   customerData.updateAt = Date.now()
+  console.log(name)
   try {
-    const createdCustomer = await customer.create(req.body)
+    const createdCustomer = await Customer.create(req.body)
     res.status(200).json({
-      msg: 'customer created',
-      data: createdCustomer
+      createdCustomer
     })
   } catch (error) {
     res.status(400).send({
@@ -29,7 +44,40 @@ const createCustomer = async (req, res) => {
   }
 }
 
+const updateCustomer = async (req, res) => {
+  const customerData = req.body
+  customerData.updateAt = new Date()
+  const { id } = req.params
+  try {
+    await Customer.updateOne({ _id: id }, req.body)
+    res.status(200).json(
+      'customer updated'
+    )
+  } catch (error) {
+    res.status(400).send({
+      msg: error
+    })
+  }
+}
+
+const deleteCustomer = async (req, res) => {
+  const { id } = req.params
+  try {
+    await Customer.findByIdAndDelete(id)
+    return res.status(200).json({
+      msg: 'invoice deleted'
+    })
+  } catch (e) {
+    return res.status(400).json({
+      msg: e
+    })
+  }
+}
+
 module.exports = {
   getAllCustomers,
-  createCustomer
+  getOneByCustomerId,
+  createCustomer,
+  updateCustomer,
+  deleteCustomer
 }
