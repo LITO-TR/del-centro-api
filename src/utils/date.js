34ac8311d6c8c6102e8c29d471a8543608fb34d0
Nilton Torres <89040283+LITO-TR@users.noreply.github.com>
2023-01-08 17:23:22 -0500

    test

--- src/utils/date.js
+++
@@ -1,8 +0,0 @@
-const date = new Date()
-const millNow = Date.now()
-// const millDiff = 18000000
-
-console.log(date.getTimezoneOffset())
-console.log(millNow)
-console.log(date.toLocaleDateString('es-ES'))
-// const date1 = new Date(millNow - millDiff)
