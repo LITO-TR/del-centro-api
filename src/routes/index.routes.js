0cb983c6ffa50ad169947e13e9083b4583cf9614
Nilton Torres <89040283+LITO-TR@users.noreply.github.com>
2023-01-08 17:23:14 -0500

    added route for create credit

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,7 +1,9 @@
 const employeeRoutes = require('../routes/employee.routes')
 const customerRoutes = require('../routes/customer.routes')
+const creditRoutes = require('./credit.routes')
 const routes = (app) => {
   app.use('/api/customers', customerRoutes)
   app.use('/api/employees', employeeRoutes)
+  app.use('/api/credit', creditRoutes)
 }
 module.exports = routes
