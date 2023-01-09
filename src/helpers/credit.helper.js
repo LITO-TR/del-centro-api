c01784a12fb62bc9d5c3086fa0bb20f5cfcf586c
Nilton Torres <89040283+LITO-TR@users.noreply.github.com>
2023-01-09 18:30:11 -0500

    added down payment terms

--- src/helpers/credit.helper.js
+++ src/helpers/credit.helper.js
@@ -1,3 +1,4 @@
+
 const plusDate = (date, days) => {
   date.setDate(date.getDate() + days)
   return date
@@ -5,9 +6,13 @@ const plusDate = (date, days) => {
 
 const getFirstDateByPaymentMethod = (paymentMethod) => {
   const date = new Date()
-
+  const day = date.getDay()
   if (paymentMethod === 'day') {
-    return plusDate(date, 1)
+    if (day === 1 || day === 2 || day === 4) {
+      return plusDate(date, 2)
+    } else if (day === 0 || day === 5 || day === 6) {
+      return plusDate(date, 1)
+    }
   } else if (paymentMethod === 'week') {
     return plusDate(date, 7)
   }
