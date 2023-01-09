2b038732bb9a0db1c631c9dccae5de557add295b
Nilton Torres <89040283+LITO-TR@users.noreply.github.com>
2023-01-08 23:36:54 -0500

    create credit extension

--- src/controllers/credit.controller.js
+++ src/controllers/credit.controller.js
@@ -5,18 +5,37 @@ const createCredit = async (req, res) => {
   creditData.interestAmount = creditData.creditAmount * creditData.decimalInterest
   creditData.totalAmount = creditData.creditAmount + creditData.interestAmount
   creditData.firstPayDay = getFirstPayDat.getFirstDateByPaymentMethod(creditData.paymentMethod)
+  creditData.discount = 0.0
+  creditData.disbursedAmount = creditData.creditAmount
   try {
     const credit = await Credit.create(req.body)
-    res.status(400).json(
+    res.status(200).json(
       credit
     )
   } catch (error) {
-    res.status(200).json({
+    res.status(400).json({
+      msg: error
+    })
+  }
+}
+
+const createCreditExtension = async (req, res) => {
+  const { id } = req.params
+  const creditData = req.body
+  creditData.disbursedAmount = creditData.creditAmount - creditData.discount
+  try {
+    const creditExtension = await Credit.updateOne(id, req.body)
+    res.status(200).json(
+      creditExtension
+    )
+  } catch (error) {
+    res.status(400).json({
       msg: error
     })
   }
 }
 
 module.exports = {
-  createCredit
+  createCredit,
+  createCreditExtension
 }
