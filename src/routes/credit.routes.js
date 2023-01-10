d9367640abb05d9e121a15d5a68ba4d2f6e7f7a5
Nilton Torres <89040283+LITO-TR@users.noreply.github.com>
2023-01-10 17:04:46 -0500

    routes for payments

--- src/routes/credit.routes.js
+++ src/routes/credit.routes.js
@@ -5,5 +5,5 @@ const creditController = require('../controllers/credit.controller')
 const router = Router()
 
 router.post('/', creditController.createCredit)
-
+router.put('/credit/:creditId/payment/:paymentNro', creditController.paymentQuota)
 module.exports = router
