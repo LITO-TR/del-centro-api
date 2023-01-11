d50b78f5872e88f0b661402db2bb2cdaf638fd09
Nilton Torres <89040283+LITO-TR@users.noreply.github.com>
2023-01-11 01:57:02 -0500

    add payments for new schema

--- src/controllers/credit.controller.js
+++ src/controllers/credit.controller.js
@@ -1,4 +1,6 @@
 const Credit = require('../models/credit.model')
+const Payment = require('../models/payment.model')
+
 const creditHelper = require('../helpers/credit.helper')
 const createCredit = async (req, res) => {
   const creditData = req.body
@@ -6,16 +8,20 @@ const createCredit = async (req, res) => {
   creditData.creditType = 'NUEVO CREDITO'
   creditData.interestAmount = creditData.creditAmount * creditData.decimalInterest
   creditData.totalAmount = creditData.creditAmount + creditData.interestAmount
+
   creditData.firstPayDate = creditHelper.getFirstDateByPaymentMethod(creditData.paymentMethod)
   creditData.discount = 0.0
   creditData.expirationDate = creditHelper.getExpirationDay(new Date(creditData.firstPayDate), creditData.numberOfPayments, creditData.paymentMethod)
   creditData.currentDate = creditHelper.plusDate(new Date(), 0)
   creditData.disbursedAmount = creditData.creditAmount
   creditData.paymentsAmount = creditData.totalAmount / creditData.numberOfPayments
-  creditData.payments = creditHelper.getPayments(new Date(creditData.firstPayDate), creditData.numberOfPayments, creditData.paymentMethod, creditData.paymentsAmount)
+  // creditData.payments = creditHelper.getPayments(new Date(creditData.firstPayDate), creditData.numberOfPayments, creditData.paymentMethod, creditData.paymentsAmount)
   creditData.debtAmount = 0.0
+
   try {
     const credit = await Credit.create(req.body)
+    await Payment.insertMany(creditHelper.getPayments(new Date(creditData.firstPayDate), creditData.numberOfPayments, creditData.paymentMethod, creditData.paymentsAmount, credit._id))
+
     res.status(200).json(
       credit
     )
@@ -50,25 +56,30 @@ const createCreditExtension = async (req, res) => {
 }
 
 const paymentQuota = async (req, res) => {
-  const { creditId, paymentNro } = req.params
-  const propertyPaid = `payments.${paymentNro}.isPaid`
-  const propertyPaymentDate = `payments.${paymentNro}.paymentDate`
-  const objTmp = {}
-  objTmp[propertyPaymentDate] = creditHelper.plusDate(new Date(), 0)
-  objTmp[propertyPaid] = true
-
+  const { creditId, paymentId } = req.params
+  const credit = await Credit.findById(creditId)
+  const objPayment = {
+    status: 'PAGADO',
+    paymentDate: creditHelper.plusDate(new Date(), 0)
+  }
+  // TO-DO : DEBT
+  const payments = []
+  payments.push(credit.paymentsAmount)
+  const objCredit = {
+    debtAmount: creditHelper.getDebt(credit.creditAmount, payments)
+  }
   try {
-    const credit = await Credit.findById(creditId)
-    console.log('entre')
-    objTmp.debtAmount = creditHelper.getDebt(credit.debtAmount, credit.paymentsAmount)
-    const creditUpdated = await Credit.updateOne({ _id: creditId }, objTmp)
+    const payment = await Payment.updateOne({ _id: paymentId }, objPayment)
+    const updatedCredit = await Credit.updateOne({ _id: creditId }, objCredit)
 
-    res.status(200).json(
-      creditUpdated
+    res.status(200).json({
+      updatedCredit,
+      payment
+    }
     )
   } catch (error) {
     res.status(400).json(
-      error
+      { msg: error }
     )
   }
 }
