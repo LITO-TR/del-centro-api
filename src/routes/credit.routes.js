7a55ccae69fb6665bc5ec207e181934f172b543b
Nilton Torres <89040283+LITO-TR@users.noreply.github.com>
2023-01-11 11:01:05 -0500

    add route for credit extension

--- src/routes/credit.routes.js
+++ src/routes/credit.routes.js
@@ -1,10 +1,10 @@
 const { Router } = require('express')
 const creditController = require('../controllers/credit.controller')
-const paymentController = require('../controllers/payment.controller')
 const router = Router()
 
 router.post('/', creditController.createCredit)
 router.put('/:creditId/payment/:paymentId', creditController.paymentQuota)
+router.post('/:idCredit/extension')
 // router.post(':idCredit/payments', paymentController)
 
 module.exports = router
