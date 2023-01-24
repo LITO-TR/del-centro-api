48053fa362838865132da462c7207a0128f8bd21
Nilton Torres <89040283+LITO-TR@users.noreply.github.com>
2023-01-24 15:48:34 -0500

    refactor credit controller

--- src/controllers/credit.controller.js
+++ src/controllers/credit.controller.js
@@ -69,43 +69,6 @@ const createCreditExtension = async (req, res) => {
   }
 }
 
-const paymentQuota = async (req, res) => {
-  const { paymentId } = req.params
-
-  const payment = await Payment.findById(paymentId)
-  const credit = await Credit.findById(payment.creditId)
-  const objPayment = {
-    status: 'PAGADO',
-    paymentDate: creditHelper.plusDate(new Date(), 0)
-  }
-  // TO-DO : DEBT
-  // validar con pagos en validators
-  let objCredit = {}
-  if (payment.status === 'PENDIENTE') {
-    objCredit = {
-      debtAmount: credit.debtAmount - credit.paymentsAmount
-    }
-  } else if (payment.status === 'PAGADO') {
-    objCredit = {
-      debtAmount: credit.debtAmount
-    }
-  }
-
-  try {
-    await Payment.updateOne({ _id: paymentId }, objPayment)
-    await Credit.updateOne({ _id: payment.creditId }, objCredit)
-    // const payment = await Payment.findById(paymentId)
-    const payments = await Payment.find({ creditId: payment.creditId })
-    console.log(payments)
-    res.status(200).json(
-      payments
-    )
-  } catch (error) {
-    res.status(400).json(
-      { msg: error }
-    )
-  }
-}
 const getAllCredits = async (req, res) => {
   try {
     const credits = await Credit.find()
@@ -145,26 +108,10 @@ const getPaymentsByCreditId = async (req, res) => {
     )
   }
 }
-
-const getPaymentsByDate = async (req, res) => { // segun la fecha?
-  const { day, month, year } = req.params
-  try {
-    const payments = await Payment.find({ date: year + '/' + month + '/' + day })
-    res.status(200).json(
-      payments
-    )
-  } catch (error) {
-    res.status(400).json(
-      error
-    )
-  }
-}
 module.exports = {
   createCredit,
   createCreditExtension,
-  paymentQuota,
   getAllCredits,
   getPaymentsByCreditId,
-  getCreditById,
-  getPaymentsByDate
+  getCreditById
 }
