c8424bc81c62d9f0c18988231849d14c4dab8929
Nilton Torres <89040283+LITO-TR@users.noreply.github.com>
2023-01-24 15:49:25 -0500

    added payments route

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,9 +1,12 @@
 const employeeRoutes = require('../routes/employee.routes')
 const customerRoutes = require('../routes/customer.routes')
 const creditRoutes = require('./credit.routes')
+const paymentRoutes = require('./payment.routes')
+
 const routes = (app) => {
   app.use('/api/customers', customerRoutes)
   app.use('/api/employees', employeeRoutes)
-  app.use('/api/credit', creditRoutes)
+  app.use('/api/credits', creditRoutes)
+  app.use('/api/payments', paymentRoutes)
 }
 module.exports = routes
