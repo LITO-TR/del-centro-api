dd7b3a2c245796ed0a1d25d502f3b0f99436feb3
Nilton Torres <89040283+LITO-TR@users.noreply.github.com>
2023-01-24 15:51:34 -0500

    refactor collision code

--- src/routes/payment.routes.js
+++ src/routes/payment.routes.js
@@ -3,11 +3,6 @@ const { Router } = require('express')
 const paymentController = require('../controllers/payment.controller')
 
 const router = Router()
-// router.get('/', creditController.getAllCredits)
-// router.post('/', creditController.createCredit)
 router.put('/:paymentId', paymentController.paymentQuota)
-// router.post('/:creditId/extension', creditController.createCreditExtension)
-// router.get('/:id/payments', creditController.getPaymentsByCreditId)
-// router.get('/:creditId', creditController.getCreditById)
 router.get('/day/:day/month/:month/year/:year', paymentController.getPaymentsByDate)
 module.exports = router
