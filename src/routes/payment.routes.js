a448bda3ba29b7018d7daa294f9bede4c6a50916
Nilton Torres <89040283+LITO-TR@users.noreply.github.com>
2023-01-30 19:34:07 -0500

    added new routes

--- src/routes/payment.routes.js
+++ src/routes/payment.routes.js
@@ -3,6 +3,6 @@ const { Router } = require('express')
 const paymentController = require('../controllers/payment.controller')
 
 const router = Router()
-router.put('/:paymentId', paymentController.paymentQuota)
+router.put('/:paymentId/method/:method/customer-payment/:customerPayment', paymentController.paymentQuota)
 router.get('/day/:day/month/:month/year/:year', paymentController.getPaymentsByDate)
 module.exports = router
